c2a86c09bde06e2ff6b8c8d5e86d6bd52595e688
Santiago Noguera <santiiago.noguera@gmail.com>
2026-06-18 18:07:30 -0300

    Add TC-26

--- cypress/e2e/myTestRestfulBooker.cy.js
+++ cypress/e2e/myTestRestfulBooker.cy.js
@@ -134,4 +134,33 @@ describe("Shady Meadows - Formulario de Contacto", () => {
       );
     });
   });
+
+  //TC-26 Verifica si frontend y backend procesan
+  //correctamente caracteres especiales en Message
+  it("26. Caracteres especiales", () => {
+    cy.intercept("POST", "/api/message").as("contactSuccess");
+
+    const mensajeEspecial =
+      "¡Hola! ¿Cómo estás? @#$%^&*()_+-=[]{}|;:',.<>/?~` ñ Ñ áéíóú";
+
+    cy.visit("https://automationintesting.online");
+
+    cy.get('[data-testid="ContactName"]').type("Juan Pérez");
+    cy.get('[data-testid="ContactEmail"]').type("juan_perez@prueba.com");
+    cy.get('[data-testid="ContactPhone"]').type("35133221231");
+    cy.get('[data-testid="ContactSubject"]').type("Pregunta sobre reserva");
+    cy.get('[data-testid="ContactDescription"]').type(mensajeEspecial);
+
+    cy.get(".d-grid > .btn").click();
+
+    cy.get(".col-lg-8 > .card > .card-body > .h4").should(
+      "contain",
+      "Thanks for getting in touch Juan Pérez!",
+    );
+
+    cy.wait("@contactSuccess").then((interception) => {
+      expect(interception.request.body.description).to.equal(mensajeEspecial);
+      expect(interception.response.statusCode).to.equal(200);
+    });
+  });
 });
